34b3f1abbc8e25882a62b71116ef2e7b7ecd73d2
Aditya <lapratomo24@yahoo.com>
2024-09-26 21:16:53 +0200

    added generate password method based on Day 5 with adjustment

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_pass():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_numbers = random.randint(2, 4)
+    nr_symbols = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for letter in range(nr_letters)]
+    password_numbers = [(random.choice(numbers)) for number in range(nr_numbers)]
+    password_symbols = [(random.choice(symbols)) for symbol in range(nr_symbols)]
+
+    pw = password_letters + password_numbers + password_symbols
+
+    random.shuffle(pw)
+    passwords = "".join(pw)
+    field3.delete(0, END)
+    field3.insert(0, f"{passwords}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -52,7 +77,7 @@ password.grid(column=0, row=3)
 field3 = Entry(width=34)
 field3.insert(END, string="")
 field3.grid(column=1, row=3)
-generate = Button(text="Generate Password")
+generate = Button(text="Generate Password", command=generate_pass)
 generate.grid(column=2, row=3)
 
 btn = Button(text="Add", command=save)
